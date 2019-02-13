3e0008d91cbf8234fe4b6828e2f771c9c88702d0
Jon Cabrera <jon.cabrera@fon.com>
2019-02-13 18:16:56 +0100

    Completed convention exercises

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other : MyDate) =
+        when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +17,25 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterator<MyDate> {
+    var current: MyDate = start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean = current <= endInclusive
+}
+
+operator fun DateRange.contains(date : MyDate) = date >= start && date <= endInclusive
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val times: Int)
+
+operator fun TimeInterval.times(number:Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) = addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.times)
+
+
+
